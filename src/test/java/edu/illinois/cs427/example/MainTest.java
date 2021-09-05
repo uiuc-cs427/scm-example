17912aa3451bdb259cfc3ba450aa00a005752987
Lingming Zhang <zhanglm10@gmail.com>
2021-09-05 14:59:01 -0500

    scm example

---
+++ src/test/java/edu/illinois/cs427/example/MainTest.java
@@ -0,0 +1,20 @@
+package edu.illinois.cs427.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Main.
+ */
+public class MainTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void test1()
+    {
+        assertEquals(3, Main.sum(1, 2));
+    }
+}
